b36f63617576193036626cc9775b6d3927c54fdc
Aleksi Dragoev <aleksidragoev@gmail.com>
2016-02-16 11:07:00 -0500

    Ruby_on_Rails/begin.rb changes

---
+++ Ruby_on_Rails/begin.rb
@@ -0,0 +1,22 @@
+# 3.times { puts "Hello World" }
+#
+# # this is a comment
+# puts 5 # so is this
+# 3 # and this
+#
+# p "Got it" # => Got it
+times_2=2
+times_2 *= 2 while times_2<100
+puts times_2
+def add(one, two)
+  one + two
+end
+
+def divide(one, two)
+  return "I don't think so" if two == 0
+  one / two
+end
+
+puts add(2, 2) # => 4
+puts divide(2, 0) # => I don't think so
+puts divide(12, 4) # => 3
