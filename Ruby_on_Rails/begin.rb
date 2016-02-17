d6845fc4103f9c027d54dcb383603c4425bda918
Aleksi Dragoev <aleksidragoev@gmail.com>
2016-02-17 10:40:15 -0500

    changes

--- Ruby_on_Rails/begin.rb
+++ Ruby_on_Rails/begin.rb
@@ -1,22 +1,32 @@
-# 3.times { puts "Hello World" }
+# # 3.times { puts "Hello World" }
+# #
+# # # this is a comment
+# # puts 5 # so is this
+# # 3 # and this
+# #
+# # p "Got it" # => Got it
+# times_2=2
+# times_2 *= 2 while times_2<100
+# puts times_2
+# def add(one, two)
+#   one + two
+# end
 #
-# # this is a comment
-# puts 5 # so is this
-# 3 # and this
+# def divide(one, two)
+#   return "I don't think so" if two == 0
+#   one / two
+# end
 #
-# p "Got it" # => Got it
-times_2=2
-times_2 *= 2 while times_2<100
-puts times_2
-def add(one, two)
-  one + two
-end
+# puts add(2, 2) # => 4
+# puts divide(2, 0) # => I don't think so
+# puts divide(12, 4) # => 3
+begin
 
-def divide(one, two)
-  return "I don't think so" if two == 0
-  one / two
-end
+  File.foreach( 'do_not_exist.txt' ) do |line|
+    puts line.chomp
+  end
 
-puts add(2, 2) # => 4
-puts divide(2, 0) # => I don't think so
-puts divide(12, 4) # => 3
+rescue Exception => e
+  puts e.message
+  puts "Let's pretend this didn't happen..."
+end
