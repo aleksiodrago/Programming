b314b21f89902d6c622616acbb298ad2f5c9ae51
Aleksi Dragoev <aleksidragoev@gmail.com>
2016-02-18 10:39:27 -0500

    Changes2

--- Ruby_on_Rails/begin.rb
+++ Ruby_on_Rails/begin.rb
@@ -20,13 +20,29 @@
 # puts add(2, 2) # => 4
 # puts divide(2, 0) # => I don't think so
 # puts divide(12, 4) # => 3
-begin
+# begin
+#
+#   File.foreach( 'do_not_exist.txt' ) do |line|
+#     puts line.chomp
+#   end
+#
+# rescue Exception => e
+#   puts e.message
+#   puts "Let's pretend this didn't happen..."
+# end
+some_range = 1..3
+puts some_range.max # => 3
+puts some_range.include? 2 # => true
 
-  File.foreach( 'do_not_exist.txt' ) do |line|
-    puts line.chomp
-  end
+puts (1...10) === 5.3 # => true
+puts ('a'...'r') === "r" # => false (end-exclusive)
 
-rescue Exception => e
-  puts e.message
-  puts "Let's pretend this didn't happen..."
+p ('k'..'z').to_a.sample(2) # => ["k", "w"]
+# or another random array with 2 letters in range
+
+age = 12
+case age
+  when 0..12 then puts "Still a baby"
+  when 13..99 then puts "Teenager at heart!"
+  else puts "You are getting older..."
 end
