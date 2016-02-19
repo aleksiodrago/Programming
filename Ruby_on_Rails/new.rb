c7799a22185ec8c509a66c5bbd2b3faad662b1ba
Aleksi Dragoev <aleksidragoev@gmail.com>
2016-02-19 10:25:42 -0500

    b

---
+++ Ruby_on_Rails/new.rb
@@ -0,0 +1,9 @@
+#This is a  ruby program that shall be used as a test for all the new things I've learned
+#Intializ variables
+reason = "random"
+string = "123"
+threewords = "I am a Rebel"
+
+#create statements
+puts "asdfghjklqwertyuiopzxcvbnm" if reason == "random"
+puts "You rebel!" if (string.is_a?(Integer)) or threewords.split.length >3
