38b16fc7ae700595d7d91c731e93b6fbcb8ec5ca
Aleksi Dragoev <aleksidragoev@gmail.com>
2016-02-22 16:34:25 -0500

    _

--- Ruby_on_Rails/new.rb
+++ Ruby_on_Rails/new.rb
@@ -7,3 +7,22 @@ threewords = "I am a Rebel"
 #create statements
 puts "asdfghjklqwertyuiopzxcvbnm" if reason == "random"
 puts "You rebel!" if (string.is_a?(Integer)) or threewords.split.length >3
+# team.rb
+class Team
+  include Enumerable # LOTS of functionality
+
+  attr_accessor :name, :players
+  def initialize (name)
+    @name = name
+    @players = []
+  end
+  def add_players (*players) # splat
+    @players += players
+  end
+  def to_s
+    "#{@name} team: #{@players.join(", ")}"
+  end
+  def each
+    @players.each { |player| yield player }
+  end
+end
